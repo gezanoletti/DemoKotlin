f705bb4d1e58f59846c01f288c1ce86122812a06
G.E. Zanoletti <gezanoletti@gmail.com>
2018-09-27 22:32:02 -0600

    First

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+package demo
+
+//https://www.youtube.com/watch?v=H_oGi8uuDpA
+fun main(args: Array<String>) {
+    println("Hello, world!")
+
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+
+    println("Biggest Int : $bigInt")
+    println("Biggest Int : $smallInt")
+}
